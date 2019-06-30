f4379debc016612043ac3d9d35816c84f6841ef0
Jenn Henderson <jenn.henderson18@gmail.com>
2019-06-30 16:41:35 -0700

    generates random word and updates blank lines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,51 @@
-// Computer randomly chooses a word from the word array
+// create an array of words
 
-var wordCatalog = ["Accio", "Azkaban", "Basilisk", "Butterbeer", "Dementor", "Deathly Hallows", "Diagon Alley", "Divination", "Dobby", "Expelliarmus", "Expecto patronum", "Firebolt", "Floo powder", "The Forbideen Forest", "Gillyweed", "Goblet of Fire", "Godric's Hollow", "Gryffindor", "Hufflepuff", "Hippogriff", "Hogsmeade", "Incendio", "Lumos", "Ministry of Magic", "Mudblood", "Muggle", "Nargles", "Nimbus Two Thousand", "Nox", "Obliviate", "Parseltongue", "Patronus", "Polyjuice Potion", "Privet Drive", "Quidditch", "Ravenclaw", "Seeker", "Slytherin", "Sorting Hat", "Squib", "Time Turner", "Transfiguration", "Wand", "Whomping Willow", "Wingardium Leviosa"]
+var wordCatalog = ["Accio", "Azkaban", "Basilisk", "Butterbeer", "Dementor", "Divination", "Dobby", "Expelliarmus", "Firebolt", "Gillyweed", "Gryffindor", "Hufflepuff", "Hippogriff", "Hogsmeade", "Incendio", "Lumos", "Mudblood", "Muggle", "Nargles", "Nox", "Obliviate", "Parseltongue", "Patronus", "Quidditch", "Ravenclaw", "Seeker", "Slytherin", "Squib", "Transfiguration", "Wand"]
 
-var currentWord = wordCatalog[Math.floor(Math.random()*wordCatalog.length)];
+// randomly select a word from the list
+
+var word = wordCatalog[Math.floor(Math.random()*wordCatalog.length)];
 
 console.log(wordCatalog)
-console.log(currentWord)
+console.log(word)
+
+// Update clue to reflect number of characters in word
+
+var answer = [];
+for (var i = 0; i < word.length; i++) {
+    answer[i] = "_";
+    console.log(answer)
+}
+
+// variable for remaining letters in word
+
+var remainingLetters = word.length;
+console.log(remainingLetters)
 
 // Assign remaining guesses to 10
-// The empty lines are updated to match the number of characters in the array
+
+var remainingGuesses = 10;
+
+
 // The user inputs a Key
 
-    document.onkeyup = function() {
-        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        console.log(userGuess)
+    document.onkeyup = function(event) {
+        var userGuess = event.key 
 ;    }
 // If that key was correct show which letter they guessed correct (can be in multiple places)
+
+
+
 // If that key was incorrect show the letter and minus one from remaining guesses
 // If the user guesses the word before the reamining guesses is 0 add 1 to their win
+
+var wins = 0;
+var loses = 0;
+
 // If the user does not guess the word correctly and their remaining guesses is 0 add plus 1 to their lose score
+
+
+
 // If the user wins or loses restart the game and randomly select another word
 
 
